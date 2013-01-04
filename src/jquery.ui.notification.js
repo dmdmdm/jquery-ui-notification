3fee9937a45b3cc451dd10e595eb5f02f830dd06
dmdmdm <dmckellar@gmail.com>
2013-01-04 15:30:16 -0500

    Update src/jquery.ui.notification.js
    I found a unminified version of the code - here it is.

--- src/jquery.ui.notification.js
+++ src/jquery.ui.notification.js
@@ -1,104 +1,320 @@
-/*
+/* Downloaded from http://jsfiddle.net/mekwall/Md98w/8/embedded/result,js,html,css/
  * jQuery UI Notification
  * Copyright (c) 2011 Marcus Ekwall
  *
  * http://writeless.se/projects/jquery-ui-notification/
  *
  * Depends:
- *   - jQuery 1.4+
- *   - jQuery UI 1.8+ widget factory
+ *   - jQuery 1.4
+ *   - jQuery UI 1.8 widget factory
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- */
- (function ($) {
-    $(document.body).is("[role]") || $(document.body).attr("role", "application");
-    var a = 0;
-    $.widget("me.notification", {
-        options: {
-            notificationClass: "ui-widget-content",
-            offset: 10,
-            duration: 2000,
+*/
+(function($) {
+
+// role=application on body required for screenreaders to correctly interpret aria attributes
+if( !$(document.body).is('[role]') ){
+    $(document.body).attr('role','application');
+}
+    
+var increments = 0;
+$.widget("me.notification", {
+    options: {
+        notificationClass: "ui-widget-content",
+        offset: 10,
+        duration: 2000,
+        show: {
+            effect: 'fade',
+            options: {},
+            speed: 250,
+            callback: function(){}
+        },
+        hide: {
+            effect: 'fade',
+            options: {},
+            speed: 250,
+            callback: function(){}
+        },
+        fancyHide: true,
+        sticky: false,
+        stack: "below"
+    },
+    content: {
+        title: "Notification",
+        content: function() {
+            return $(this).attr("title");
+        },
+        template: "<h4>${Title}</h4><p>${Content}</p>",
+    },
+    _init: function(){
+        // container for notifications
+        this.notifications = [];
+        
+        // notification dummy
+        this.notification = $("<div></div>")
+            .attr("role", "notification")
+            .attr("aria-hidden", "true")
+            .addClass("ui-notification ui-widget ui-corner-all")
+            .hide();
+        
+        if (this.options.stack == "below") {
+            this.notification.css('marginBottom', this.options.offset);
+        } else {
+            this.notification.css('marginTop', this.options.offset);
+        }
+        
+        // notification close button dummy
+        this.notificationClose = $("<a/>")
+            .attr("href", "#")
+            .attr("role", "button")
+            .addClass("ui-notification-close ui-corner-all")
+            .hover(function(){
+                $(this).addClass("ui-state-hover");
+            }, function(){
+                $(this).removeClass("ui-state-hover");
+            })
+            .append(
+                $("<span></span>")
+                    .addClass("ui-icon ui-icon-closethick")
+                    .text("close")
+            );
+        
+        // notification content dummy
+        this.notificationContent = $("<div></div>")
+            .addClass("ui-notification-content");
+    },
+    
+    enable: function() {
+        this.options.disabled = false;
+    },
+    
+    disable: function() {
+        this.options.disabled = true;
+    },
+    
+    widget: function(index) {
+        return index ? this.notifications[index] : this.notifications;
+    },
+    
+    index: function() {
+        return increments;
+    },
+
+    create: function(content, options) {
+        var self = this;
+        
+        // get defaults
+        content = $.extend({}, this.content, content);
+        
+        // override options if necessary
+        options = $.extend({}, this.options, options);
+        
+        if (options.disabled) {
+            return;
+        }
+
+        // create a new notification instance
+        this.notifications.push(new $.me.notification.instance(this)._create(content, $.extend({}, this.options, options)));
+    }
+});
+    
+// instance constructor
+$.extend($.me.notification, {
+    instance: function(widget){
+        this.parent = widget;
+    }
+});
+    
+// notification instance
+$.extend($.me.notification.instance.prototype, {
+    _create: function(content, options){
+        this.options = options;
+        
+        // render content template
+        var content = content.template
+            .replace("${Title}", content.title)
+            .replace("${Content}", content.content);
+        
+        // create notification element
+        this.element = this.parent.notification.clone(true).attr("id", "ui-notification-"+increments++)
+            // append the content
+            .append(this.parent.notificationContent.clone().html(content))
+            // add custom class
+            .addClass(options.notificationClass);
+        
+        if (options.stack == "below") {
+            this.element.appendTo(this.parent.element);
+        } else {
+            this.element.prependTo(this.parent.element);
+        }
+
+        // if sticky, add close button
+        var self = this;
+        if (options.sticky) {
+            this.parent.notificationClose
+                .clone(true)
+                .click(function(event){
+                    self.hide();
+                    return false;
+                })
+                .prependTo(this.element);
+        } else {
+            // otherwise set timeout to close it automatically
+            setTimeout(function(){self.hide();}, self.options.duration);
+        }
+        
+        // let's show the notifiction
+        this.show();
+        
+        // return instance
+        return this;
+    },
+    show: function(){
+        var anim = this.options.show;
+        this.element
+            .attr("aria-hidden", "false")
+            .show(
+                anim.effect, 
+                anim.options, 
+                anim.speed, 
+                anim.callback
+            );
+    },
+    hide: function(){
+        var self = this;
+        var anim = this.options.hide;
+        var wrap = $("<div></div>").height(this.element.outerHeight(true));
+        
+        if (this.options.fancyHide) {
+            this.element.wrap(wrap);
+        }
+
+        this.element.hide(
+            anim.effect,
+            anim.options,
+            anim.speed,
+            function(){
+                if (self.options.fancyHide) {
+                    $(this).parent().animate({ height: 0},
+                        250,
+                        function(){
+                            $(this).remove();
+                        }
+                    );
+                } else {
+                    $(this).remove();
+                }
+                if (anim.callback) {
+                    anim.callback();
+                }
+            }
+        );
+    }
+    
+});
+    
+})( jQuery );
+
+$(function(){
+    
+    $(".ui-notification-container").notification();
+    $(".ui-notification-bottomleft").notification({
+        stack: "above"
+    });
+    $(".ui-notification-bar").notification({
+        notificationClass: "ui-state-highlight",
+        offset: 0,
+        sticky: true,
+        fancyHide: false,
+        show: {
+            effect: 'blind',
+            options: {
+                direction: 'vertical'
+            },
+            speed: 250,
+            callback: function(){}
+        },
+        hide: {
+            effect: 'blind',
+            options: {
+                direction: 'vertical'
+            },
+            speed: 250,
+            callback: function(){}
+        },
+    });
+    
+    $("button.default").click(function(){
+    
+        $(".ui-notification-container").notification("create", {
+            title: "Hello world!",
+            content: "I am a default notification"
+        });
+    
+    });
+    
+    $("button.sticky").click(function(){
+    
+        $(".ui-notification-container").notification("create", {
+            title: "Hello world!",
+            content: "I am a sticky notification"
+        },
+        {
+            sticky: true 
+        });
+    
+    });
+    
+    $("button.custom-anim").click(function(){
+    
+        $(".ui-notification-container").notification("create", {
+            title: "Hello world!",
+            content: "I am a sticky notification"
+        },
+        {
             show: {
-                effect: "fade",
+                effect: 'bounce',
                 options: {},
                 speed: 250,
-                callback: function () {}
+                callback: function(){}
             },
             hide: {
-                effect: "fade",
+                effect: 'clip',
                 options: {},
                 speed: 250,
-                callback: function () {}
-            },
-            fancyHide: true,
-            sticky: false,
-            stack: "below"
-        },
-        content: {
-            title: "Notification",
-            content: function () {
-                return $(this).attr("title")
-            },
-            template: "<h4>${Title}</h4><p>${Content}</p>"
-        },
-        _init: function () {
-            this.notifications = $("<div></div>").attr("role", "notification").attr("aria-hidden", "true").addClass("ui-notification ui-widget ui-corner-all").hide(), this.notification = $("<div></div>").attr("role", "notification").attr("aria-hidden", "true").addClass("ui-notification ui-widget ui-corner-all").hide(), this.options.stack == "below" ? this.notification.css("marginBottom", this.options.offset) : this.notification.css("marginTop", this.options.offset), this.notificationClose = $("<a/>").attr("href", "#").attr("role", "button").addClass("ui-notification-close ui-corner-all").hover(function () {
-                $(this).addClass("ui-state-hover")
-            }, function () {
-                $(this).removeClass("ui-state-hover")
-            }).append($("<span></span>").addClass("ui-icon ui-icon-closethick").text("close")), this.notificationContent = $("<div></div>").addClass("ui-notification-content")
-        },
-        enable: function () {
-            this.options.disabled = false
-        },
-        disable: function () {
-            this.options.disabled = true
-        },
-        widget: function (a) {
-            return a ? this.notifications[a] : this.notifications
-        },
-        index: function () {
-            return a
-        },
-        create: function (a, b) {
-            var c = this;
-            a = $.extend({}, this.content, a), b = $.extend({}, this.options, b), b.disabled || this.notifications.push((new $.me.notification.instance(this))._create(a, $.extend({}, this.options, b)))
-        }
-    }), $.extend($.me.notification, {
-        instance: function (a) {
-            this.parent = a
-        }
-    }), $.extend($.me.notification.instance.prototype, {
-        _create: function (b, c) {
-            var d = this;
-            this.options = c;
-            var b = b.template.replace("${Title}", b.title).replace("${Content}", b.content);
-            this.element = this.parent.notification.clone(true).attr("id", "ui-notification-" + a++).append(this.parent.notificationContent.clone().html(b)).addClass(c.notificationClass), c.stack == "below" ? this.element.appendTo(this.parent.element) : this.element.prependTo(this.parent.element), c.sticky ? this.parent.notificationClose.clone(true).click(function (a) {
-                d.hide();
-                return false
-            }).prependTo(this.element) : setTimeout(function () {
-                d.hide()
-            }, d.options.duration), this.show();
-            return this
-        },
-        show: function () {
-            var a = this.options.show;
-            this.element.attr("aria-hidden", "false").show(a.effect, a.options, a.speed, a.callback)
-        },
-        hide: function () {
-            var a = this,
-                b = this.options.hide,
-                c = $("<div></div>").height(this.element.outerHeight(true));
-            this.options.fancyHide && this.element.wrap(c), this.element.hide(b.effect, b.options, b.speed, function () {
-                a.options.fancyHide ? $(this).parent().animate({
-                    height: 0
-                }, 250, function () {
-                    $(this).remove()
-                }) : $(this).remove(), b.callback && b.callback()
-            })
-        }
-    })
-})(jQuery)
+                callback: function(){}
+            }
+        });
+    
+    });
+    
+    $("button.error").click(function(){
+         $(".ui-notification-container").notification("create", {
+            title: "Error",
+            content: "This is an error notification"
+         }, { sticky: true, notificationClass: "ui-state-error" });
+    });
+    
+    $("button.bottomleft").click(function(){
+         $(".ui-notification-bottomleft").notification("create", {
+            title: "Bottom left!",
+            content: "I will stack above"
+         });
+    });
+    
+    $("button.bar").click(function(){
+        var n = $(".ui-notification-bar").notification("create", {
+            title: "Notification bar",
+            content: "I am a notification bar"
+        }).notification("index");
+        
+        var notification = $(".ui-notification-bar").notification("widget", n);
+        console.log(notification);
+    });
+
+    $('#switcher').themeswitcher();
+});
+
